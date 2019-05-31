bae316d032b1dda8a0e36c6f1533aa541a70623c
abtin <abtin@10.0.1.9>
2019-05-31 12:24:51 -0400

    Added "look" and "drop" to commandwords.java

--- src/com/bayviewglen/zork/CommandWords.java
+++ src/com/bayviewglen/zork/CommandWords.java
@@ -13,7 +13,7 @@ class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String validCommands[] = {
-        "go", "quit", "help", "eat", "inventory", "take", "use"
+        "go", "quit", "help", "eat", "inventory", "take", "use", "look", "drop"
     };
 /**
      * Constructor - initialise the command words.
